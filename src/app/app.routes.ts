7bca680780e14f8f9eb8f9f4f58640dc6d2d999f
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-03-07 14:16:13 +0800

    Deleted Home Folder and added Auth & Tabs page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth',
     pathMatch: 'full',
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
   },
 ];
