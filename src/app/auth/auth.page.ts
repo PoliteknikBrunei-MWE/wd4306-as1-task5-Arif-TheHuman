6a08c71f17a782306307b77422a32babea1c2ed5
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-03-27 13:12:56 +0800

    Update login form in auth page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from './auth.service';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
@@ -19,7 +19,8 @@ export class AuthPage implements OnInit {
   ngOnInit() {
   }
 
-  onLogin() {
+  onLogin(form: NgForm) {
+    console.log(form);
     this.authService.login();
     this.router.navigateByUrl('/tabs/places');
   }
